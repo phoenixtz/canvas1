94a383007987b4abac4025d6285b5b2dbd25ec10
phoenixtz <shotentxz@gmail.com>
2016-04-08 09:25:26 -0500

    Se agrego descarga y multiples puntos

--- canvas.js
+++ canvas.js
@@ -2,38 +2,31 @@
 var canvas;
 var context;
 var canvasWidth = 400;
-var canvasHeight = 400;
-var padding = 25;
-var lineWidth = 8;
+var canvasHeight = 500;
+var radio = 3;
 var clickX = new Array();
 var clickY = new Array();
 var clickDrag = new Array();
+var num = new Array();
 var paint = false;
 var outlineImage = new Image();
-var drawingAreaX = 111;
-var drawingAreaY = 11;
-var drawingAreaWidth = 300;
-var drawingAreaHeight = 386;
+var drawingAreaX = canvasWidth/4;
+var drawingAreaY = 10;
+var drawingAreaWidth = 209;
+var drawingAreaHeight = 416;
 function prepareCanvas() {
-
     canvas = document.getElementById("canvas");
     context = canvas.getContext("2d");
 
-    $('#canvas').mousedown(function (e) {
-
+    $('#canvas').mousedown(function (e)
+    {
         var mouseX = e.pageX - this.offsetLeft;
         var mouseY = e.pageY - this.offsetTop;
-
         paint = true;
         addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
         redraw();
     });
-    $('#canvas').mousemove(function (e) {
-        if (paint) {
-            addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
-            redraw();
-        }
-    });
+
     $('#canvas').mouseup(function (e) {
         paint = false;
     });
@@ -41,47 +34,46 @@ function prepareCanvas() {
     $('#canvas').mouseleave(function (e) {
         paint = false;
     });
-    outlineImage.src = "cuerpo.png";
+    outlineImage.src = "silueta.png";
     outlineImage.onload = checkImage;
 }
 function checkImage(){
     context.drawImage(outlineImage, drawingAreaX, drawingAreaY, drawingAreaWidth, drawingAreaHeight);
 }
-function addClick(x, y, dragging)
+function addClick(x, y)
 {
     clickX.push(x);
     clickY.push(y);
-    clickDrag.push(dragging);
+
 }
 
 function redraw()
 {
-
     context.strokeStyle = "black";
-    context.lineJoin = "round";
-    context.lineWidth = 10;
-    clearCanvas();
-    //context.drawImage(outlineImage, drawingAreaX, drawingAreaY, drawingAreaWidth, drawingAreaHeight);
+    context.drawImage(outlineImage, drawingAreaX, drawingAreaY, drawingAreaWidth, drawingAreaHeight);
     for (var i = 0; i < clickX.length; i++)
     {
+        num[i] = i+1;
         context.beginPath();
-        context.drawImage(outlineImage, drawingAreaX, drawingAreaY, drawingAreaWidth, drawingAreaHeight);
-        if(clickDrag[i] && i)
-        {
-            context.moveTo(clickX[i-1], clickY[i-1]);
-        }
-        else
-        {
-            context.moveTo(clickX[i]-1, clickY[i]);
-        }
-        //context.lineTo(clickX[i],clickY[i]);
-        context.arc(clickX[i],clickY[i],2,0,(Math.PI * 2),false);
+        context.moveTo(clickX[i]-1, clickY[i]);
+        context.fillStyle = 'black';
+        context.arc(clickX[i],clickY[i],radio,0,(Math.PI * 2),false);
+        context.fill();
         context.closePath();
         context.stroke();
+        context.beginPath();
+        context.font = '14px Arial';
+        context.fillStyle = 'red';
+        context.fillText(""+num[i], clickX[i], clickY[i]);
+        context.closePath();
     }
-
 }
 function clearCanvas()
 {
     context.clearRect(0, 0, canvasWidth, canvasHeight);
+    checkImage();
+    clickX = [];
+    clickY = [];
+    clickDrag = [];
 }
+
